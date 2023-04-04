11f361fea8ea5aa8b434a765c7d1d722f6dfd50c
Suhail Chouhan <suhail.chouhan@programmers.io>
2023-04-04 12:20:18 +0530

    make basic route setup and pages

---
+++ src/components/header.vue
@@ -0,0 +1,24 @@
+<template>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-menu-button color="primary"></ion-menu-button>
+        </ion-buttons>
+        <ion-title>{{ $route.name }}</ion-title>
+      </ion-toolbar>
+    </ion-header>
+</template>
+
+<script>
+import {IonHeader,IonToolbar, IonButtons, IonMenuButton, IonTitle} from '@ionic/vue';
+    export default {
+      name:'HeaderComponent',
+        components:{
+            IonHeader,IonToolbar, IonButtons, IonMenuButton, IonTitle
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,21 @@
 import { createRouter, createWebHistory } from "@ionic/vue-router";
 
 const routes = [
+
   {
-    path: "",
-    redirect: "/folder/Inbox",
+    path: "/events",
+    component: ()=> import("../views/EventsList.vue"),
+    name: 'Events'
   },
   {
-    path: "/folder/:id",
-    component: () => import("../views/FolderPage.vue"),
+    path: "/login",
+    component: () => import("../views/Login.vue"),
+    name: 'Login'
+  },
+  {
+    path: "/profile",
+    component: () => import("../views/Profile.vue"),
+    name: 'Profile'
   },
 ];
 
